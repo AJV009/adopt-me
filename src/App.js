e223de708118244bdbb1dd81b639f22c12f9f714
Alphons Jaimon <jaimonalphons@gmail.com>
2022-01-06 05:40:26 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="havanese" />
-      <Pet name="pepper" animal="bird" breed="cockatiel" />
-      <Pet name="Sudo" animal="dog" breed="wheaten terrier" />
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
